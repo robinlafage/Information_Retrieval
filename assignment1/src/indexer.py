bc03846c37cce06707b481c23861697eb4382df5
robinlafage <robinlafage42@gmail.com>
2024-10-03 10:38:07 +0100

    class Parser

--- assignment1/src/indexer.py
+++ assignment1/src/indexer.py
@@ -1,8 +1,24 @@
+from parser import *
+
 class Indexer:
-    def __init__(self, inputFile, outputFile):
+    def __init__(self, inputFile, outputFile, minimumTokenLength):
         self.inputFile = inputFile
         self.outputFile = outputFile
-
+        self.minimumTokenLength = minimumTokenLength
 
     def buildIndex(self):
-        pass
+        start = 0
+        end = 10
+
+        parser = Parser(None)
+
+        with open(self.inputFile, "r") as file:
+            for i, line in enumerate(file):
+                if i < start:
+                    continue 
+                if i >= end:
+                    break 
+                parser.line = line
+                parser.parse()
+                print(f"DocId: {parser.docId}")
+                print(f"Text: {parser.text}")
