0c4c15117053c172a0d963723ade5c3087b7f636
robinlafage <robinlafage42@gmail.com>
2024-10-03 15:53:11 +0100

    parts of index are written on disk

--- assignment1/src/indexer.py
+++ assignment1/src/indexer.py
@@ -1,24 +1,60 @@
 from parser import *
+from tokenizer import *
+import json
 
 class Indexer:
     def __init__(self, inputFile, outputFile, tokenizerOptions):
         self.inputFile = inputFile
         self.outputFile = outputFile
         self.tokenizerOptions = tokenizerOptions
+        self.tempDict = {}
 
     def buildIndex(self):
-        start = 0
-        end = 10
+        blocSize = 1000
+        i = 0
+        currentBloc = 0
 
         parser = Parser(None)
+        tokenizer = Tokenizer(None, self.tokenizerOptions["minimumTokenLength"], " .,;\'\"-_`@~\\/?!()[]\{\}", [])
 
         with open(self.inputFile, "r") as file:
-            for i, line in enumerate(file):
-                if i < start:
-                    continue 
-                if i >= end:
-                    break 
+            for line in file:
+
                 parser.line = line
                 parser.parse()
-                print(f"DocId: {parser.docId}")
-                print(f"Text: {parser.text}")
+                tokenizer.string = parser.text
+                tokens = tokenizer.tokenize()
+                self.buildDictionary(tokens, parser.docId)
+
+                i += 1
+                if i >= blocSize:
+                    self.writeTempDictInDisk(currentBloc)
+                    self.tempDict = {}
+                    i = 0
+                    currentBloc += 1
+
+        if self.tempDict:
+            self.writeTempDictInDisk(currentBloc)
+            self.tempDict = {}
+
+
+    def buildDictionary(self, tokens, docId):
+
+        i = 0
+        for tok in tokens:
+            if tok not in self.tempDict:
+                self.tempDict[tok] = {docId: [i]}
+            else:
+                if docId not in self.tempDict[tok]:
+                    self.tempDict[tok][docId] = [i]
+                else:
+                    self.tempDict[tok][docId].append(i)
+
+            i += 1
+                
+
+
+    def writeTempDictInDisk(self, currentBloc):
+        self.tempDict = dict(sorted(self.tempDict.items()))
+        with open(f"../index/indexPart{currentBloc}.json", "w") as file:
+            file.write(json.dumps(self.tempDict))
--- assignment1/src/tokenizer.py
+++ assignment1/src/tokenizer.py
@@ -27,12 +27,12 @@ class Tokenizer:
         if token != "" and len(token) > self.minimumTokenLength:
             tokenList.append(token)
 
-        print(tokenList)
+        # print(tokenList)
         return tokenList
 
 if __name__ == "__main__" :
     tokenizer = Tokenizer("Organization of the genes encoding complement receptors type 1 and 2, decay-accelerating factor, and C4-binding protein in the RCA locus on human chromosome 1.\n\nThe organization and physical linkage of four members of a major complement locus, the RCA locus, have been determined using the technique of pulsed field gradient gel electrophoresis in conjunction with Southern blotting. The genes encoding CR1, CR2, DAF, and C4bp were aligned in that order within a region of 750 kb. In addition, the 5' to 3' orientation of the CR1 gene (5' proximal to CR2) was determined using 5'- and 3'-specific DNA probes. The proximity of these genes may be related to structural and functional homologies of the protein products. Overall, a restriction map including 1,500 kb of DNA was prepared, and this map will be important for positioning of additional coding sequences within this region on the long arm of chromosome 1.", \
                             3, \
-                            " .,;\'\"-_`@~\\/?!()[]\{\}"\
+                            " .,;\'\"-_`@~\\/?!()[]\{\}",\
                             ["or","the"])
     tokenizer.tokenize()
