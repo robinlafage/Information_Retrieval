80bf31b218c931d74d9ef482c4759813cde59ebf
Valentin RENIER <valentin.renier@etu.esisar.grenoble-inp.fr>
2024-10-10 15:32:52 +0100

    Modifying name in stemmer to use the same syntax

--- Assignment 1/src/stemmer.py
+++ Assignment 1/src/stemmer.py
@@ -1,12 +1,12 @@
 import nltk.stem.porter as stemmerLibrary
 
 class Stemmer:
-    def __init__(self,normalizeToLowerCase,tokens_list):
+    def __init__(self,normalizeToLowerCase,tokenList):
         self.normalizeToLowerCase = normalizeToLowerCase
-        self.tokens_list = tokens_list
+        self.tokenList = tokenList
 
     def stem(self):
-        return [stemmerLibrary.PorterStemmer().stem(token, self.normalizeToLowerCase) for token in self.tokens_list]
+        return [stemmerLibrary.PorterStemmer().stem(token, self.normalizeToLowerCase) for token in self.tokenList]
 
 if __name__ == "__main__" :
     stemmer = Stemmer(False, ["I", "show", "off", "of", "this"])
