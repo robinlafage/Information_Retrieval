d659ffee3675b779c4e5790f0e002610f1f11b06
Valentin RENIER <valentin.renier@etu.esisar.grenoble-inp.fr>
2024-10-10 10:52:33 +0100

    Adding the stemmer Modifying a bit of the tokenizer Update on the readme

--- Assignment 1/src/stemmer.py
+++ Assignment 1/src/stemmer.py
@@ -1,8 +1,14 @@
-#We can use every type of stemmer
-#Kstem or Porter are best algo
-#Dictionary based is slower, but could be better if the dictionary is good enough
+import nltk.stem.porter as stem 
 
 class Stemmer:
-    def __init__(self, token):
-        self.token = token
+    def __init__(self,normalizeToLowerCase,tokens_list):
+        self.normalizeToLowerCase = normalizeToLowerCase
+        self.tokens_list = tokens_list
 
+    def stemming(self):
+        return [stem.PorterStemmer().stem(token, self.normalizeToLowerCase) for token in self.tokens_list]
+
+if __name__ == "__main__" :
+    stemmer = Stemmer(False, ["I", "show", "off", "of", "this"])
+    tokens = stemmer.stemming()
+    print(' '.join(tokens))
--- Assignment 1/src/tokenizer.py
+++ Assignment 1/src/tokenizer.py
@@ -1,3 +1,5 @@
+from stemmer import *
+
 class Tokenizer:
     def __init__(self, string, minimumTokenLength, normalizeToLowerCase, cuttingList, stopWordsPath):
         self.string = string
@@ -5,7 +7,7 @@ class Tokenizer:
         self.normalizeToLowerCase = normalizeToLowerCase
         self.cuttingList = cuttingList
         self.stopWordsPath = stopWordsPath
-        #StopWords won't work if I don't set the lowercase earlier
+        #StopWords won't work if we don't set the lowercase before checking if it's in the StopWords list
 
     def tokenize(self):
         token = ""
@@ -19,6 +21,8 @@ class Tokenizer:
             #TODO Implement special rules for characters in cuttingList ?
             #Exemple : "pique-nique" should be 1 or 2 tokens ?
             #Exemple : "Jhon's" should be 1 or 2 tokens ? If 2 tokens, "'s" should be "is" or "'s" ?
+
+            #TODO U.S.A have to be USA 
             elif token in stopWords :
                 token = ""
             elif len(token) < self.minimumTokenLength :
@@ -31,7 +35,9 @@ class Tokenizer:
                 if token != "" :
                     tokenList.append(token)
                     token=""
-        if token != "" and len(token) > self.minimumTokenLength:
+        if token != "" \
+           and len(token) > self.minimumTokenLength\
+           and token not in stopWords:
             tokenList.append(token)
 
         #TODO Delete the prints
@@ -39,9 +45,13 @@ class Tokenizer:
         return tokenList
 
 if __name__ == "__main__" :
+    normalizeToLowerCase = True
     tokenizer = Tokenizer("Organization of the genes encoding complement receptors type 1 and 2, decay-accelerating factor, and C4-binding protein in the RCA locus on human chromosome 1.\n\nThe organization and physical linkage of four members of a major complement locus, the RCA locus, have been determined using the technique of pulsed field gradient gel electrophoresis in conjunction with Southern blotting. The genes encoding CR1, CR2, DAF, and C4bp were aligned in that order within a region of 750 kb. In addition, the 5' to 3' orientation of the CR1 gene (5' proximal to CR2) was determined using 5'- and 3'-specific DNA probes. The proximity of these genes may be related to structural and functional homologies of the protein products. Overall, a restriction map including 1,500 kb of DNA was prepared, and this map will be important for positioning of additional coding sequences within this region on the long arm of chromosome 1.", \
                             3,\
-                            True,\
+                            normalizeToLowerCase,\
                             " .,;\'\"-_`@~\\/?!()[]\{\}\n",\
                             "../stopwords-en.txt")
-    tokenizer.tokenize()
+    tokens = tokenizer.tokenize()
+    stemmer = Stemmer(normalizeToLowerCase, tokens)
+    print(str(stemmer.stemming()))
+    
