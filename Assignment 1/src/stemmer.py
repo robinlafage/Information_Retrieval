1c5dcf8ef1cc69e6a95c2047e5e91adc669ad2e2
Valentin RENIER <valentin.renier@etu.esisar.grenoble-inp.fr>
2024-10-13 00:41:36 +0100

    Modified the stemmer to include original token Modified the main to make booleans arguments to work Suppressed the stemming from the tokenizer Added the stemming into the indexer Optimized the stemming in the indexer file to avoid stemming an identical token twice

--- Assignment 1/src/stemmer.py
+++ Assignment 1/src/stemmer.py
@@ -1,14 +1,18 @@
 import nltk.stem.porter as stemmerLibrary
 
 class Stemmer:
-    def __init__(self,normalizeToLowerCase,tokenList):
-        self.normalizeToLowerCase = normalizeToLowerCase
+    def __init__(self,tokenList):
         self.tokenList = tokenList
 
     def stem(self):
-        return [stemmerLibrary.PorterStemmer().stem(token, self.normalizeToLowerCase) for token in self.tokenList]
+        tokenListAfterStemming = []
+        for token in self.tokenList :
+            #We never let the stemmer option to normalize to lower, always the work of tokenizer
+            tokenAfterStemming = stemmerLibrary.PorterStemmer().stem(token, False)
+            tokenListAfterStemming.append([token, tokenAfterStemming])
+        return tokenListAfterStemming
 
 if __name__ == "__main__" :
-    stemmer = Stemmer(False, ["I", "show", "off", "of", "this"])
-    tokens = stemmer.stemming()
-    print(' '.join(tokens))
+    stemmer = Stemmer(["I", "showed", "these", "off", "of", "this"])
+    tokens = stemmer.stem()
+    print(str(tokens))
