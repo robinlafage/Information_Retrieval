fb58a576e0e8f42b63e243b4b07576a31d988bd5
robinlafage <robinlafage42@gmail.com>
2024-11-14 10:09:58 +0000

    documentatioon and remove unused code

--- Assignment 1/src/searcher/ndcgMetric.py
+++ Assignment 1/src/searcher/ndcgMetric.py
@@ -11,6 +11,12 @@ class NDCG:
 
 
     def computeMetric(self):
+        """
+        Calculate the nDCG metric for all queries in the queries file, using the scores file
+
+        Returns:
+            float: The average nDCG@10
+        """
         k = 10
         totalNdcg = 0
 
@@ -28,10 +34,23 @@ class NDCG:
                 i += 1
 
         self.metric = totalNdcg / i
-        print(f"Average nDCG@{k}: {self.metric}")
+
+        return self.metric
 
 
     def calculateNdcg(self, results, relevant_set, k):
+        """
+        Calculate the nDCG metric for a query
+
+        Args:
+            results (list): List of retrieved documents
+            relevant_set (set): Set of relevant documents
+            k (int): Number of documents to consider
+
+        Returns:
+            float: The nDCG@k
+        """
+
         y_true = [1 if doc in relevant_set else 0 for doc in results[:k]]
         y_score = [1 / (i + 1) for i in range(len(results[:k]))] 
 
